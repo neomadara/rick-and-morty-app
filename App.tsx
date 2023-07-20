3c87f4bb9bfca37caf21aea9c76aa51593755ce5
Cristian Gutierrez <instanciageek@gmail.com>
2023-07-19 21:29:43 -0400

    no-history: move pages files to view in src

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Home from './src/pages/Home';
+import Home from './src/views/Home';
 
 const queryClient = new QueryClient();
 const App = () => (
